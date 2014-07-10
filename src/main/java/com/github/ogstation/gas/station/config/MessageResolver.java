53067a37be59787de468d496a04b99e862791c14
Wen Hao <wenhao@126.com>
2014-07-10 13:49:35 +0800

    [GAS-STATION][Wen Hao] - restructured.

--- src/main/java/com/github/ogstation/gas/station/config/MessageResolver.java
+++
@@ -1,8 +0,0 @@
-package com.github.ogstation.gas.station.config;
-
-public interface MessageResolver
-{
-    String getMessage(String code);
-
-    String getMessage(String code, Object[] params);
-}
