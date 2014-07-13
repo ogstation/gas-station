e1f24aa5ca7f4424e9f0d49c0bdfcddf250e1357
Hao Wen <wenhao@126.com>
2014-07-13 17:54:12 +0800

    [GAS_STATION][Wen Hao] - added service tests.

--- src/test/java/com/github/ogstation/gas/station/controller/GasStationControllerTest.java
+++ src/test/java/com/github/ogstation/gas/station/controller/GasStationControllerTest.java
@@ -27,7 +27,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
--- src/test/java/com/github/ogstation/gas/station/service/GasStationServiceImplTest.java
+++ src/test/java/com/github/ogstation/gas/station/service/GasStationServiceImplTest.java
@@ -1,8 +1,77 @@
 package com.github.ogstation.gas.station.service;
 
+import com.github.ogstation.gas.station.domain.Station;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.data.domain.Pageable;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 public class GasStationServiceImplTest
 {
 
+    private GasStationServiceImpl gasStationService;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        gasStationService = new GasStationServiceImpl();
+    }
+
+    @Test
+    public void should_be_able_to_get_station() throws Exception
+    {
+        // given
+
+        // when
+        gasStationService.get("key");
+
+        // then
+    }
+
+    @Test
+    public void should_be_able_to_get_stations() throws Exception
+    {
+        // given
+        Pageable pageable = mock(Pageable.class);
+
+        // when
+        gasStationService.get(pageable);
+
+        // then
+    }
+
+    @Test
+    public void should_be_able_to_create() throws Exception
+    {
+        // given
+
+        // when
+        gasStationService.create(new Station());
+
+        // then
+    }
+
+    @Test
+    public void should_be_able_to_update() throws Exception
+    {
+        // given
+
+        // when
+        gasStationService.update(new Station());
+
+        // then
+    }
+
+    @Test
+    public void should_be_able_delete() throws Exception
+    {
+        // given
+
+        // when
+        gasStationService.delete("key");
+
+        // then
+    }
 }
